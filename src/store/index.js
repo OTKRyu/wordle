3ca6e52a57be3e1786bf950cf1a279323d51e8d7
OTKRyu <appn12@gmail.com>
2022-09-08 22:55:39 +0900

    feat: add basic statistics data to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,41 @@ import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    totalWordleCount: 0,
+    totalWinCount: 0,
+    tmpTime: 0,
+    totalTrial: 0,
+    wordles: {},
+  },
+  mutations: {
+    addTotalWordleCount(state) {
+      state.totalWordleCount += 1;
+    },
+    addTotalWinCount(state) {
+      state.totalWinCount += 1;
+    },
+    setTmpTime(state, newTime) {
+      state.tmpTime = newTime;
+    },
+    addTotalTrial(state, trial) {
+      state.totalTrial += trial;
+    },
+  },
+  actions: {
+    patchTotalWordleCount({ commit }) {
+      commit("addTotalWordleCount");
+    },
+    patchTotalWinCount({ commit }) {
+      commit("addTotalWinCount");
+    },
+    patchTmpTime({ commit }, newTime) {
+      commit("setTmpTime", newTime);
+    },
+    patchTotalTrial({ commit }, trial) {
+      commit("addTotalTrial", trial);
+    },
+  },
   modules: {},
   plugins: [createPersistedState()],
 });
